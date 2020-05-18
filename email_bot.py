90cc3f67cd054e1766b8bbe92aa71f27474191a9
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-05-18 16:37:45 -0300

    Add subject validation with regex and catch errors

--- email_bot.py
+++ email_bot.py
@@ -1,5 +1,6 @@
 import os
 import smtplib
+import re
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -15,29 +16,36 @@ app = Flask(__name__)
 
 @app.route('/send', methods=['POST'])
 def send_email():
-    # Initialize session
-    email = os.environ.get(EMAIL_ENV)
-    password = os.environ.get(PASSWORD_ENV)
+    try:
+        # Initialize session
+        email = os.environ.get(EMAIL_ENV)
+        password = os.environ.get(PASSWORD_ENV)
 
-    session = authenticate_email(email, password)
+        session = authenticate_email(email, password)
 
-    # Get data from request
-    req_data = request.get_json()
+        # Get data from request
+        req_data = request.get_json()
 
-    recipient = req_data['recipient']
-    text = req_data['text']
-    subject = req_data['subject']
+        recipient = req_data['recipient']
+        text = req_data['text']
+        subject = req_data['subject']
 
-    message = create_message(recipient, subject)
-    message.attach(MIMEText(text, 'plain'))
-    
-    body = message.as_string()
+        if is_subject_valid(subject) is False:
+            return {'error': 'Invalid email body'}
 
-    # Send the email
-    session.sendmail(email, recipient, body)
-    session.quit()
+        message = create_message(recipient, subject)
+        message.attach(MIMEText(text, 'plain'))
+        
+        body = message.as_string()
 
-    return {'message': 'Email sent succesfully!'}
+        # Send the email
+        session.sendmail(email, recipient, body)
+        session.quit()
+
+        return {'message': 'Email sent succesfully!'}
+
+    except:
+        return {'error': 'There was an error processing the request'}
 
 
 def authenticate_email(email, password):
@@ -55,3 +63,24 @@ def create_message(recipient, subject):
     message['Subject'] = subject
 
     return message
+
+
+def is_subject_valid(subject):
+    # subject = ["Function", "'{function_name}'", "execution", "log"]
+    subject_text = subject.split()
+
+    valid_subject = [r"Function",
+                     # <function name>
+                     # [a-zA-Z_] = First char must be a letter or _
+                     # [a-zA-Z_\d]* = The following chars can be either letters, numbers or _
+                     r"'[a-zA-Z_][a-zA-Z_\d]*'",
+                     r"execution",
+                     r"log"]
+
+    for text, pattern in zip(subject_text, valid_subject):
+        match = re.match(pattern, text)
+
+        if match is None:
+            return False
+
+    return True
